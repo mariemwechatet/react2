af35885fec4f753d4ebce83f1339153ed7eb4c10
mariemwechatet <wechtetimariem@gmail.com>
2021-09-05 18:00:31 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import jsx from "./images/jsx.jpg"
+import  "./style.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className="titleRed">  React JSX</h1><br/>
+     <img className="img1" src="/images/img1.png" alt="react"/><br/>
+     
+     <img className="img2" src ={jsx} alt ="jsxp"/><br/>
+     <video  controls  src="/vidéo/video.mp4"/>
+
     </div>
+
   );
 }
 
